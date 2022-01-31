0b5fe93c1650af02ecf6b730f1990b2d1fe01c47
Berkay Çatak <berkaypng@gmail.com>
2022-01-31 14:09:50 +0300

    proc_open error

--- config/flare.php
+++ config/flare.php
@@ -25,12 +25,12 @@ return [
     */
 
     'reporting' => [
-        'anonymize_ips' => true,
+        'anonymize_ips' => false,
         'collect_git_information' => false,
-        'report_queries' => true,
+        'report_queries' => false,
         'maximum_number_of_collected_queries' => 200,
-        'report_query_bindings' => true,
-        'report_view_data' => true,
+        'report_query_bindings' => false,
+        'report_view_data' => false,
         /*'grouping_type' => null,
         'report_logs' => true,
         'maximum_number_of_collected_logs' => 200,
