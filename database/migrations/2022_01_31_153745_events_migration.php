733d43a510f15f1e110525799ca28edfcc360e15
Berkay Çatak <berkaypng@gmail.com>
2022-01-31 18:45:03 +0300

    events_migration created

---
+++ database/migrations/2022_01_31_153745_events_migration.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EventsMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('adress')->nullable();
+            $table->date('date');
+            $table->char('time', 6);
+            $table->integer('created_user_id');
+            $table->char('is_live', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
