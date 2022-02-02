7e8a622eea9476ecbd83636bc4ed78f6f509fd64
Berkay Çatak <berkaypng@gmail.com>
2022-02-02 23:22:42 +0300

    Added missed events.

--- resources/views/pages/events/list.blade.php
+++ resources/views/pages/events/list.blade.php
@@ -43,9 +43,54 @@
                     </div>
                 </div>
             @endforeach
-            {{ $events->links() }}
             @if(count($events) == 0 )
                 <span>Henüz hiç etkinlik yok. <a href="{{ route('event.create') }}">Hemen oluştur!</a></span>
             @endif
         </div>
+
+    <div class="main-header">
+        <h1>Geçmiş Etkinlikler</h1>
+    </div>
+    <div class="main-events">
+        @foreach($past_events as $event)
+            <div class="event-card">
+                <a href="{{ route('event.show', $event->id) }}"><h3>{{ $event->name }}</h3></a>
+                <div class="event-card-spec">
+                    <div class="ecs-item">
+                        <span>⏰</span>
+                        <span class="ecs-item-text"> {{ $event->time }}</span>
+                    </div>
+                    <div class="ecs-item">
+                        <span>🗓</span>
+                        <span class="ecs-item-text">{{ $event->date }}</span>
+                    </div>
+                    <div class="ecs-item">
+                        @isset(Auth::user()->id)
+                            @if($event->created_user_id == Auth::user()->id)
+                                <a href="{{ route('event.edit', $event->id) }}">Etkinliği düzenle</a>
+                            @endif
+                        @endif
+                    </div>
+                    <div class="ecs-item">
+                        <a href="{{ route('event.show', $event->id) }}">Etkinliğe git</a>
+                    </div>
+                    @isset(Auth::user()->id)
+                        @if($event->created_user_id == Auth::user()->id)
+                            <div class="ecs-item">
+                                <form method="POST" action="{{ route('event.destroy', $event->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button onclick="return confirm('Silmek istediğinizden emin misiniz ?')" class="danger-button" type="submit">Etkinliği sil</button>
+                                </form>
+                            </div>
+                        @endif
+                    @endif
+                </div>
+            </div>
+        @endforeach
+        {{ $past_events->links() }}
+        @if(count($events) == 0 )
+            <span>Henüz hiç etkinlik yok. <a href="{{ route('event.create') }}">Hemen oluştur!</a></span>
+        @endif
+    </div>
 </x-app-layout>
